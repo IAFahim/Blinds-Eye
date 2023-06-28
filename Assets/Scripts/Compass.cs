0b33bdbccfca9280df650aaf16047db3c78366d9
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2023-06-28 14:48:19 +0600

    Adding A compass

--- Assets/Scripts/Compass.cs
+++ Assets/Scripts/Compass.cs
@@ -1,30 +1,17 @@
+using TMPro;
 using UnityEngine;
 
 public class Compass : MonoBehaviour
 {
-    public RectTransform compass_image;
-
-    void Start()
-    {
-        if (Input.compass.enabled)
-        {
-            Input.compass.enabled = true;
-        }
-        else
-        {
-            gameObject.SetActive(false);
-        }
-    }
-
+    public ARCompass arCompass;
+    public RectTransform compassImage;
+    public TMP_Text text;
     void Update()
     {
-        if (Input.compass.enabled)
-        {
-            // Get the current heading from the compass
-            float heading = Input.compass.trueHeading;
-
-            // Rotate the minimap in the opposite direction of the heading
-            compass_image.transform.rotation = Quaternion.Euler(0f, 0f, -heading);
-        }
+        Quaternion quaternion = arCompass.TrueHeadingRotation;
+        quaternion = Quaternion.Euler(0, 0, -quaternion.eulerAngles.y);
+        compassImage.rotation = quaternion;
+        Vector3 eulerAngles = quaternion.eulerAngles;
+        text.text = $"x: {eulerAngles.x} y: {eulerAngles.y} z: {eulerAngles.z}";
     }
 }
---
+++ Assets/Scripts/WebCam.cs
@@ -0,0 +1,91 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class WebCam : MonoBehaviour
+{
+    public RawImage rawImageBackground;
+    public WebCamTexture _cameraTexture;
+    public TMP_Text text;
+    public bool _isCamAvaible;
+    public AspectRatioFitter aspectRatioFitter;
+    public Button RefetchButton;
+    public Slider width;
+    public Slider height;
+    public Slider oriantation;
+    public Vector2 dimensions;
+    public Button flip;
+    public bool flipped = false;
+
+    private void Start()
+    {
+        SetUpCam();
+        RefetchButton.onClick.AddListener(SetUpCam);
+        flip.onClick.AddListener(flipIt);
+    }
+
+    private void SetUpCam()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (devices.Length == 0)
+        {
+            text.text = "No Camera Detected";
+            Debug.Log("No Camera Detected");
+            _isCamAvaible = false;
+            return;
+        }
+
+        for (int i = 0; i < devices.Length; i++)
+        {
+            if (devices[i].isFrontFacing == false)
+            {
+                _cameraTexture = new WebCamTexture(devices[i].name, (int)width.value, (int)height.value, 30);
+                Debug.Log($"x: {width.value} y: {(int)height.value}");
+                break;
+            }
+        }
+
+        _isCamAvaible = true;
+        _cameraTexture.Play();
+
+        rawImageBackground.texture = _cameraTexture;
+    }
+
+    private void Update()
+    {
+        UpdateCameraRender();
+    }
+
+    private void flipIt()
+    {
+        flipped = !flipped;
+    }
+
+    private void UpdateCameraRender()
+    {
+        if (_isCamAvaible == false)
+        {
+            return;
+        }
+
+        float ratio = 1;
+        if (flipped)
+        {
+             ratio = (float)_cameraTexture.width / (float)_cameraTexture.height;
+        }
+        else
+        {
+             ratio = (float)_cameraTexture.height / (float)_cameraTexture.width;
+        }
+
+        // aspectRatioFitter.aspectRatio = ratio;
+
+        // int orientation = _cameraTexture.videoRotationAngle;
+        text.text =
+        $"width: {_cameraTexture.width} height: {_cameraTexture.height} orientation: {_cameraTexture.videoRotationAngle}";
+        // orientation = (int)(orientation * oriantation.value);
+        // rawImageBackground.rectTransform.localEulerAngles = new Vector3(0, 0, orientation);
+    }
+}
